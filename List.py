45fa319ed6d6a32b680816a47e37d209572cc0e9
Anvi-2010900 <61372149+Anvi-2010900@users.noreply.github.com>
2020-04-11 14:43:47 -0700

    Create List.py

---
+++ List.py
@@ -0,0 +1,7 @@
+desserts = ['cookie', 'icecream', 'dounut', 'macaroon', 'macaron', 'cupcake', 'cake', 'popsicle', 'strudel', 'pie', 'cake pops', 'mochi', 'tart', 'creampuffs']
+print(desserts)
+desserts = []
+print(desserts)
+desserts += ['cookie', 'icecream', 'dounut', 'macaroon', 'macaron', 'cupcake', 'cake', 'popsicle', 'strudel', 'pie', 'cake pops', 'mochi', 'tart', 'creampuffs']
+desserts = ['cheesecake']
+print(desserts)
